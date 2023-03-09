3b3f8d9575a6e81f57791de598d996dcb6b3f6cd
Slava-png <pylyp.sviatoslav@gmail.com>
2023-03-09 11:53:01 +0100

    implemente solution

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,40 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        // dealing with x cords
+        changeDirectionForX(robot, toX);
+        while (toX != robot.getX()) {
+            robot.stepForward();
+        }
+
+        // dealing with y cords
+        changeDirectionForY(robot, toY);
+        while (toY != robot.getY()) {
+            robot.stepForward();
+        }
+    }
+
+    public void changeDirectionForX(Robot robot, int toX) {
+        if (toX > robot.getX()) {
+            while (robot.getDirection() != Direction.RIGHT) {
+                robot.turnLeft();
+            }
+        } else {
+            while (robot.getDirection() != Direction.LEFT) {
+                robot.turnRight();
+            }
+        }
+    }
+
+    public void changeDirectionForY(Robot robot, int toY) {
+        if (toY > robot.getX()) {
+            while (robot.getDirection() != Direction.UP) {
+                robot.turnLeft();
+            }
+        } else {
+            while (robot.getDirection() != Direction.DOWN) {
+                robot.turnRight();
+            }
+        }
     }
 }
